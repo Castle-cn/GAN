efbd7c704872b34de19486b6d5e2e7d6114d38f4
Castle-cn <2803270399@qq.com>
2023-04-09 18:03:21 +0800

    aa

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Generator(nn.Module):
         if len(img_size) > 2:
             raise ValueError("img_size should be (H,W) of the train image")
         self.H, self.W = img_size
-        self.generator = models.resnet50(pretrained=False,
+        self.generator = models.resnet18(weights=None,
                                          num_classes=self.H * self.W)
 
     # 输入x是随机噪声，大小为224*224
@@ -17,12 +17,13 @@ class Generator(nn.Module):
         x = self.generator(x)  # 输出的就是图片打平后
         return x.reshape((self.H, self.W))
 
+
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.discriminator = models.resnet50(pretrained=False,
-                                         num_classes=1)
+        self.discriminator = models.resnet18(weights=None,
+                                             num_classes=1)
 
-    def forward(self,x):
+    def forward(self, x):
         x = self.discriminator(x)
         return x
