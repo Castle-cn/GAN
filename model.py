b388583936ba59dd10c405adf2c7a408cc3f5f12
Castle-cn <2803270399@qq.com>
2023-04-10 18:48:23 +0800

    aa

--- model.py
+++ model.py
@@ -8,15 +8,15 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.stack = nn.Sequential(
             nn.Linear(g_input_dim, 256),
-            nn.BatchNorm1d(256),
+            # nn.BatchNorm1d(256),
             nn.LeakyReLU(),
 
             nn.Linear(256, 512),
-            nn.BatchNorm1d(512),
+            # nn.BatchNorm1d(512),
             nn.LeakyReLU(),
 
             nn.Linear(512, 1024),
-            nn.BatchNorm1d(1024),
+            # nn.BatchNorm1d(1024),
             nn.LeakyReLU(),
 
             nn.Linear(1024, g_output_dim),
@@ -34,63 +34,25 @@ class Discriminator(nn.Module):
     def __init__(self, d_input_dim):
         super(Discriminator, self).__init__()
 
-        self.relu = nn.LeakyReLU()
-        self.sigmod = nn.Sigmoid()
+        self.stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(d_input_dim, 1024),
+            # nn.BatchNorm1d(1024),
+            nn.LeakyReLU(),
 
-        self.flat = nn.Flatten()
-        self.bn0 = nn.BatchNorm1d(d_input_dim)
-        self.linear1 = nn.Linear(d_input_dim, 1024)
-        self.bn1 = nn.BatchNorm1d(1024)
-        self.linear2 = nn.Linear(1024, 512)
-        self.bn2 = nn.BatchNorm1d(512)
-        self.linear3 = nn.Linear(512, 256)
-        self.bn3 = nn.BatchNorm1d(256)
-        self.linear4 = nn.Linear(256, 1)
+            nn.Linear(1024, 512),
+            # nn.BatchNorm1d(512),
+            nn.LeakyReLU(),
 
-        # self.stack = nn.Sequential(
-        #     nn.Flatten(),
-        #     nn.Linear(d_input_dim, 1024),
-        #     nn.BatchNorm1d(1024),
-        #     nn.LeakyReLU(),
-        #
-        #     nn.Linear(1024, 512),
-        #     nn.BatchNorm1d(512),
-        #     nn.LeakyReLU(),
-        #
-        #     nn.Linear(512, 256),
-        #     nn.BatchNorm1d(256),
-        #     nn.LeakyReLU(),
-        #
-        #     nn.Linear(256, 1),
-        #     nn.Sigmoid()
-        # )
+            nn.Linear(512, 256),
+            # nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
     # forward method
     def forward(self, x):
-        # x = self.stack(x)
-
-        x = self.flat(x)
-        x = self.linear1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.linear2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
-
-        x = self.linear3(x)
-        x = self.bn3(x)
-        x = self.relu(x)
-
-        x = self.linear4(x)
-        x = self.sigmod(x)
-
+        x = self.stack(x)
         return x
-
-
-# gen_model = Generator((28, 28))
-# model = Discriminator(28 * 28)
-# for i in range(2):
-#     data = torch.randn((32, 28 * 28))
-# out = model(data)
-# print(out)
--- test.py
+++ test.py
@@ -4,78 +4,31 @@ import torch.nn.functional as F
 from matplotlib import pyplot as plt
 import torch
 
-
-def cross_entropy(predictions, targets):
-    predictions = np.clip(predictions, 1e-15, 1 - 1e-15)  # 防止取 log 时出现零值或负数
-    ce = -np.mean(targets * np.log(predictions) + (1 - targets) * np.log(1 - predictions))
-    return ce
-
-
 if __name__ == '__main__':
-    a = np.asarray([[7.5476e-05],
-                    [4.9661e-05],
-                    [8.0105e-05],
-                    [1.1499e-04],
-                    [7.5657e-05],
-                    [7.2256e-05],
-                    [7.2581e-05],
-                    [3.9100e-05],
-                    [7.3301e-05],
-                    [2.7205e-05],
-                    [4.4697e-05],
-                    [3.1211e-05],
-                    [3.2596e-05],
-                    [4.8518e-05],
-                    [4.2086e-05],
-                    [3.8372e-05],
-                    [4.4195e-05],
-                    [5.8393e-05],
-                    [8.7155e-05],
-                    [8.2982e-05],
-                    [3.2820e-05],
-                    [6.0196e-05],
-                    [6.7776e-05],
-                    [3.6987e-05],
-                    [3.8355e-05],
-                    [3.0987e-05],
-                    [4.6070e-05],
-                    [7.9648e-05],
-                    [6.8070e-05],
-                    [9.0252e-05],
-                    [2.1299e-05],
-                    [6.5475e-05]])
-    a = np.squeeze(np.reshape(a, [1, -1]))
-    b = np.zeros_like(a)
-    print(cross_entropy(a, b))
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
 
-    a = torch.from_numpy(a)
-    b = torch.from_numpy(b)
-    print(F.cross_entropy(a, b))
-    # device = 'cpu'
-    # if torch.cuda.is_available():
-    #     device = 'cuda'
-    #
-    # discriminator = Discriminator().to(device)
-    # discriminator.load_state_dict(torch.load('E:\desktop\dis_model_weights_30.pth'))
-    # discriminator.eval()
-    #
-    # generator = Generator().to(device)
-    # generator.load_state_dict(torch.load('E:\desktop\gen_model_weights_30.pth'))
-    # generator.eval()
-    # noise = torch.normal(mean=0, std=1, size=[10, 100]).to(device)
-    #
-    # gen = generator(noise)
-    # score = discriminator(gen)
-    #
-    # gen = (gen.cpu().detach().numpy() + 1) / 2
-    # # print(gen.shape)
-    #
-    #
-    # score = torch.flatten(score)
-    # gen_loss = F.mse_loss(torch.ones_like(score), score)
-    # print(torch.ones_like(score))
-    # print(score)
-    # print(gen_loss)
-    #
-    # plt.imshow(gen[0].squeeze(), cmap='gray')
-    # plt.show()
+    discriminator = Discriminator(28 * 28).to(device)
+    discriminator.load_state_dict(torch.load('E:\desktop\dis_model_weights_81.pth'))
+    discriminator.eval()
+
+    generator = Generator(100, 28 * 28).to(device)
+    generator.load_state_dict(torch.load('E:\desktop\gen_model_weights_81.pth'))
+    generator.eval()
+    noise = torch.normal(mean=0, std=1, size=[10, 100]).to(device)
+
+    gen = generator(noise)
+    score = discriminator(gen)
+
+    gen = (gen.cpu().detach().numpy() + 1) / 2
+    # print(gen.shape)
+
+    score = torch.flatten(score)
+    gen_loss = F.cross_entropy(score,torch.ones_like(score))
+
+    print(score)
+    print(gen_loss)
+
+    plt.imshow(gen[0].reshape((28, 28)), cmap='gray')
+    plt.show()
