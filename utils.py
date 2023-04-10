43a4be4edfc48e764b91ca8de8c9b975d7378cdc
Castle-cn <2803270399@qq.com>
2023-04-10 13:50:24 +0800

    aa

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 from torch import nn
 import torch
-
+import torch.nn.functional as F
 
 class DiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, real_img_score, gen_img_score):
-        v_real = torch.log(real_img_score).mean()
-        v_gen = torch.log(1 - gen_img_score).mean()
-        return -(v_real + v_gen)
+        real_loss = F.cross_entropy(torch.ones_like(real_img_score),real_img_score)
+        gen_loss = F.cross_entropy(torch.zeros_like(gen_img_score),gen_img_score)
+        return real_loss + gen_loss
 
 
 class GeneratorLoss(nn.Module):
@@ -17,5 +17,5 @@ class GeneratorLoss(nn.Module):
         super().__init__()
 
     def forward(self, gen_img_score):
-        v_gen = torch.log(gen_img_score).mean()
-        return -v_gen
+        gen_loss = F.cross_entropy(torch.ones_like(gen_img_score),gen_img_score)
+        return gen_loss
